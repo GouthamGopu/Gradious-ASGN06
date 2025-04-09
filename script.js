b6b7b5e56574f7bf1119eba4cfebd74308fc7937
GouthamGopu <gopugoutham3@gmail.com>
2025-04-09 13:49:58 +0530

    final

--- script.js
+++ script.js
@@ -41,24 +41,3 @@ form.addEventListener("submit", function (e) {
     alert("Your details have been successfully stored in the local storage.");
   }
 });
-
-// Instructions: 
-// ❖ The objective of this assignment is Validation. 
-// ❖ Kindly reuse the form you built in HTML & CSS Assignment (Form Design). Below, we have listed some of the fields from your form. Please ensure validation for these fields. 
-// ❖ Validate the fields using JavaScript regular expressions (Regex); Do not rely on HTML validation. 
-// ➤ Use <input type="text"> for mobile, email, number, name, website, url etc.. 
-// ➤ Do not use the "required" attribute in the input field. 
-// ❖ Validation must be performed for all the specified fields. If a field is empty or in an incorrect format, the corresponding message should be displayed through a warning message should appear at the bottom of the respective input field. 
-// Each form contains a button at the bottom, such as Submit, Apply, Next, etc. After performing all the validation When the user clicks on that button, you need to store all the user input to the browser's local storage and show a popup that says, 'Your details have been successfully stored in the local storage'. 
-// When the user reloads the page, you need to populate the user-entered information in the form. 
-// How to store the data in the local storage? 
-// var firstName = document.getElementById('txtFirstName').value; 
-// localStorage['firstName'] = firstName; 
-// (or) 
-// localStorage.setItem('firstName', firstName);
-// How to retrieve the data from localStorage? 
-// console.log(localStorage['firstName']);
-// localStorage.getItem('firstName');
-//  11. Flight Booking Form: Full Name, Email, Phone Number, Departure City, Destination City, Terms and Conditions Checkbox
-// (or) 
-// console.log(localStorage.getItem('firstName')); 
